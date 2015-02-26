ba45279fb116edd190a41b92ca12547c81d01f09
Chris Whong <chris.whong@socrata.com>
2015-02-25 22:22:55 -0500

    Adding History Chart to FrontPage

--- public/js/script.js
+++ public/js/script.js
@@ -44,7 +44,7 @@ var currentData,
       for (var i = 0; i < currentData.length; i++) {
 
         var marker = currentData[i];
-        console.log(marker);
+        //console.log(marker);
         var markerItem = L.circleMarker(
           [marker.location.lat,marker.location.lon], {
             radius: 4,
@@ -210,3 +210,109 @@ function countVehicles(route_id) {
   }
   return count;
 }
+
+//D3 Line Chart based on http://bl.ocks.org/mbostock/3902569
+var margin = {top: 20, right: 20, bottom: 40, left: 40},
+    width = $('#chartBox').width() - margin.left - margin.right,
+    height = $('#chartBox').height() - margin.top - margin.bottom;
+
+    console.log(height);
+
+var parseDate = d3.time.format.iso.parse,
+    bisectDate = d3.bisector(function(d) { return d.timestamp; }).left;
+    // formatValue = d3.format(",.2f"),
+    // formatCurrency = function(d) { return "$" + formatValue(d); };
+
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .interpolate("basis") 
+    .x(function(d) { return x(d.timestamp); })
+    .y(function(d) { return y(d.count); });
+
+var svg2 = d3.select("#chartBox").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("/history", function(error, data) {
+  console.log(data.data);
+
+  data = data.data;
+
+  data.forEach(function(d) {
+
+    d.timestamp = parseDate(d.timestamp);
+
+  });
+
+  data.sort(function(a, b) {
+    return a.timestamp - b.timestamp;
+  });
+
+  x.domain([data[0].timestamp, data[data.length - 1].timestamp]);
+  y.domain(d3.extent(data, function(d) { return d.count; }));
+
+  svg2.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg2.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Buses Reporting");
+
+  svg2.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+
+  var focus = svg2.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+  focus.append("circle")
+      .attr("r", 4.5);
+
+  focus.append("text")
+      .attr("x", 9)
+      .attr("dy", ".35em");
+
+  svg2.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  function mousemove() {
+    var x0 = x.invert(d3.mouse(this)[0]),
+        i = bisectDate(data, x0, 1),
+        d0 = data[i - 1],
+        d1 = data[i],
+        d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0;
+    focus.attr("transform", "translate(" + x(d.timestamp) + "," + y(d.count) + ")");
+    focus.select("text").text(d.count);
+  }
+});
+
